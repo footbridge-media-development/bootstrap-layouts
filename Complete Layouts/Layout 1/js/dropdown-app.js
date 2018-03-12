b36bbfd0f5378bdb54cdcd469b87c05ff32f3eb2
Garet C. Green <garetgreen1996@gmail.com>
2018-03-12 08:15:57 -0500

    Update navigation and organize CSS

--- Complete Layouts/Layout 1/js/dropdown-app.js
+++ Complete Layouts/Layout 1/js/dropdown-app.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	
 	// Drop down functionality
-	$(".has-dropdown").mouseenter(function(){
+	$(".dropdown").parent().children("a").mouseenter(function(){
 		var container = $(this).parent();
-		var menu = container.find(".dropdown");
-		if (container.length > 0){
+		var menu = container.children(".dropdown");
+		if (container.length > 0 && !menu.is(":visible")){
 			// Dropdown exists
 			menu.show("fast");
 		}
 	});
 	
-	$(".has-dropdown").each(function(index, element){
+	$(".dropdown").parent().children("a").each(function(index, element){
 		element = $(element); // Make the element a jQuery object
 		var container = element.parent();
-		var menu = container.find(".dropdown");
+		var menu = container.children(".dropdown");
 		container.mouseleave(function(){
 			menu.hide("fast");
 		})

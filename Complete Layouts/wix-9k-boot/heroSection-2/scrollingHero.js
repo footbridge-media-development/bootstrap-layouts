b0e9de64869898046eff821a4e7ba2082340c528
Garet C. Green <garetgreen1996@gmail.com>
2018-04-18 13:04:08 -0500

    Images preloading (lazyLoad)

--- Complete Layouts/wix-9k-boot/heroSection-2/scrollingHero.js
+++ Complete Layouts/wix-9k-boot/heroSection-2/scrollingHero.js
@@ -1,7 +1,18 @@
 
     // Hero Slider Controller
+	
+	// function to preload images to avoid flashing
+	function preload(url){
+		var img = new Image();
+		img.src = url;
+	}
 
     function heroSlider(obj) {
+		
+		// preload all of them
+		$.each(obj.images, function(i, url){
+			preload(url);
+		});
         
         obj.heroContainer.css('background', 'url(' + obj.images[0] + ')');
           

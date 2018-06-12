e6de7c77d8cc243c4d773367ed2aaec0121d071e
Garet C. Green <garetgreen1996@gmail.com>
2018-06-12 08:16:24 -0500

    Make menus only animate height. Fixes #11  -  Also remove old TODO comment

--- Complete Layouts/Base Layout/js/navigation-manager-app.js
+++ Complete Layouts/Base Layout/js/navigation-manager-app.js
@@ -1,98 +1,98 @@
 $(document).ready(function(){
-	
-	// TODO -> fix for dropdown not re-appearing when mouse re-enters too quickly is not to check if it's visible but to simply call hide() without the animation before re-displaying it again.
-	
+
+	var dropdownOpenSpeed = 200; // in milliseconds
+
 	// ***********************************
 	// Code for large-screen dropdowns
 	// ***********************************
 		$(".dropdown").parent()/*.children("a")*/.mouseenter(function(){
 			var container = $(this);//.parent();
 			var menu = container.children(".dropdown");
-			if (container.length > 0 && !menu.is(":visible")){
+			if (container.length > 0 && menu.css("visibility") === "hidden"){
 				// Dropdown exists
-				menu.show("fast");
-			}else if (menu.is(":visible")){
+				menu.css("visibility", "visible").css("height", 0).animate({height: menu.get(0).scrollHeight}, dropdownOpenSpeed);
+			}else if (menu.css("visibility") === "visible"){
 				// The dropdown is currently visible, but the mouse entered? This means it was trying to close.
 				// Close it immediately then open it again.
-				menu.hide();
-				menu.show("fast");
+				menu.css("visibility", "hidden");
+				menu.css("visibility", "visible").css("height", 0).animate({height: menu.get(0).scrollHeight}, dropdownOpenSpeed);
 			}
 		});
-		
+
 		$(".dropdown").parent().children("a").each(function(index, element){
 			element = $(element); // Make the element a jQuery object
 			var container = element.parent();
 			var menu = container.children(".dropdown");
 			container.mouseleave(function(){
-				menu.hide();
+				menu.css("visibility", "hidden");
 			})
 		});
-		
+
 	// ***********************************
 	// Code for small-screen dropdowns
 	// ***********************************
-	
+
 	$(".tap-dropdown").parent().children("a").click(function(e){
 		e.preventDefault(); // Prevent being linked somewhere
-		
+
 		var menu = $(this).parent().children(".tap-dropdown");
 		menu.toggle("fast");
-		
+
 		// Close any sub-menus inside this sub-menu as well
 		menu.find(".tap-dropdown").hide();
-		
+
 		return false; // Prevent being directed somewhere on older browser (idk which)
 	});
-	
+
 	// ***********************************
 	// Code for opening navigation menus
 	// ***********************************
-	
+
 		// ***********************************
 		// Small navigation menu 1
 		// ***********************************
-		
+
 			// Does this menu exist?
-			
+
 			if ($("#small-navigation-container-1").length > 0){
-				
+
 				function closeSmallMenu1(){
 					var backdrop = $("#small-navigation-container-1 .pullout-menu-backdrop");
 					var navContainer = backdrop.children("div");
-					
+
 					// Close the menu
 					navContainer.css("margin-right", navContainer.attr('data-original-margin-right'));
-					
+
 					setTimeout(function(){
 						backdrop.removeClass("d-flex");
 						backdrop.addClass("d-none");
 					}, 150); // The 150ms timeout here is because the CSS animation is set to 150ms. If you change the CSS animation speed also change the timeout here.
 				}
-		
+
 				// Open hamburger pressed
 				$("#small-navigation-menu-opener-1").click(function(){
-					var container = $(this).parent().parent().parent(); 
+					var container = $(this).parent().parent().parent();
 					var backdrop = container.find(".pullout-menu-backdrop"); // This is the transparent black backdrop
 
 					// Upon opening, remove d-none
 					backdrop.removeClass("d-none");
-					
+
 					// Add d-flex
 					backdrop.addClass("d-flex");
-					
+
 					// Get the actual container of the nav links
 					var navContainer = backdrop.children("div");
 					console.log(navContainer);
-					
+
 					// Change the margin. CSS will animate it into view
 					navContainer.attr('data-original-margin-right', navContainer.css("margin-right")).css("margin-right", 0);
 				});
-				
+
 				// Clicked on the transparent backdrop! Close the menu
 				$("#small-navigation-container-1").find(".pullout-menu-backdrop").click(function(){
 					closeSmallMenu1();
 				});
-				
+
 				// Disallow bubbling
 				$("#small-navigation-container-1").find(".pullout-menu-backdrop").children("div").click(function(e){
 					// This stops the bubbling.
@@ -105,7 +105,7 @@ $(document).ready(function(){
 					e.stopPropagation();
 					return false;
 				});
-				
+
 				// Close the small menu on resize.
 				$(window).on('resize', function(){
 					// timeout because of the "window resize" event fires before the resize happens
@@ -116,6 +116,6 @@ $(document).ready(function(){
 						}
 					}, 50);
 				});
-	
+
 			}
 });

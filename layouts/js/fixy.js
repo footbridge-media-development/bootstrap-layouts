aead7493153527dd02d61503cbde3c5743cff4e8
Garet C. Green <garetgreen1996@gmail.com>
2019-04-12 09:28:44 -0500

    Fix FixY.js - Doesn't work properly
    Remove the console log spam and allow for a #pusher div to be added below navigation to fix the fact changing the nav to an absolute positioned element changes the document height. This causes massive flickering on interior pages.

--- layouts/js/fixy.js
+++ layouts/js/fixy.js
@@ -1,14 +1,11 @@
 $(window).scroll(function (event) {
 	var scroll = $(window).scrollTop();
 	if (scroll > 50) {
-		$("#large-navigation-container-2").addClass("turnME"); 
-		console.log('fired add class');
-	}
-		
-	else {
+		let largeNavHeight = $("#large-navigation-container-2").outerHeight();
+		$("#large-navigation-container-2").addClass("turnME").css("top", 0);
+		$("#pusher").height(largeNavHeight);
+	}else{
 		$("#large-navigation-container-2").removeClass("turnME");
-		console.log('fired remove')
-		}
-	
+		$("#pusher").height(0);
+	}
 });
-

ea544247e8c10f16c9805f99da2821a7b72ada12
DrPersson <hepe22@hotmail.com>
2019-07-01 10:20:37 -0500

    Fixed broken fixy script
    The fixy script caused issues on short pages as well as stuttering when scrolling past the menu threshold. This has been fixed and simply requires a .pusher div after the navigation.

--- layouts/js/fixy.js
+++ layouts/js/fixy.js
@@ -1,11 +1,14 @@
 $(window).scroll(function (event) {
 	var scroll = $(window).scrollTop();
-	if (scroll > 50) {
-		let largeNavHeight = $("#large-navigation-container-2").outerHeight();
-		$("#large-navigation-container-2").addClass("turnME").css("top", 0);
-		$("#pusher").height(largeNavHeight);
-	}else{
-		$("#large-navigation-container-2").removeClass("turnME");
-		$("#pusher").height(0);
+	if (scroll > 200) {
+		var navH = $("#large-navigation-container-2").outerHeight();
+		$("#large-navigation-container-2").addClass("turnME");
+		$(".pusher").css({"height": navH});
 	}
+		
+	else {
+		$("#large-navigation-container-2").removeClass("turnME");
+		$(".pusher").css({"height": ""});
+		}
+	
 });

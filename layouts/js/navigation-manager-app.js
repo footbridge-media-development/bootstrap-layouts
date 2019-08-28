788eb6923cf9c37716ce7f6387415300acf52d6c
Garet C. Green <garetgreen1996@gmail.com>
2019-08-28 13:24:37 -0500

    Working iteration of removing jQuery from navigation

--- layouts/js/navigation-manager-app.js
+++ layouts/js/navigation-manager-app.js
@@ -1,200 +1,189 @@
-$(document).ready(function(){
-	var dropdownOpenSpeed = 200; // in milliseconds
+(function(){
 
-	// ***********************************
-	// Code for large-screen dropdowns
-	// ***********************************
-		function turnRGBToRGBA(colorString, alpha){
-			if (colorString.indexOf("rgba") !== -1){
-				console.log("Already is rgba");
-				return colorString.replace(/[\d\.]+\)/, alpha + ")"); // Already rgba
-			}else{
-				// Assume RGB
-				if (alpha !== undefined){
-					colorString = colorString.replace(")", ", " + alpha + ")").replace("rgb", "rgba");
-				}else{
-					colorString = colorString.replace(")", "0)").replace("rgb", "rgba");
-				}
-				return colorString
-			}
+	let dropdownSpeed = 175;
+	let allDropdowns = document.getElementsByClassName("dropdown");
+	let allTapDropdowns = document.getElementsByClassName("tap-dropdown");
+	let smallNavContainer1 = document.getElementById("small-navigation-container-1");
+
+	/**
+	* Gets the collective height of all direct children of an element
+	*
+	* Because the scrollHeight property of an element includes absolute-positioned
+	* children's height, this function exists to get the the total height of all
+	* direct children.
+	*
+	* @param {DOMNode} dropdown - The dropdown div
+	* @return {number}
+	*/
+	function getTotalHeightOfAllDirectChildren(dropdown){
+		let totalHeight = 0;
+		let children = dropdown.children;
+
+		for (let element of children){
+			totalHeight += element.clientHeight;
 		}
 
-		function adjustNavFixedOpacity(forceFullOpacity){
-			var windowScrollThreshold = 400; // How far to scroll, in pixels, before full opacity
-			var currentScroll = $(window).scrollTop();
-			var percentageScrolled = currentScroll / windowScrollThreshold;
+		return totalHeight;
+	}
 
-			if (percentageScrolled > 1 || forceFullOpacity === true){
-				percentageScrolled = 1;
-			}
+	/**
+	* Opens a tap-dropdown
+	*
+	* @param {DOMNode} tapDropdownContainer - The tap-dropdown div
+	* @return {void}
+	*/
+	function openTapDropdown(tapDropdownContainer){
+		tapDropdownContainer.style.overflow = "hidden";
+		tapDropdownContainer.style.display = "block";
+		let childrenHeight = getTotalHeightOfAllDirectChildren(tapDropdownContainer);
 
-			var colorString = turnRGBToRGBA($(".nav-fixed").css("background-color"), percentageScrolled);
-			console.log("Setting color of nav to " + colorString);
-
-			$(".nav-fixed").css("background-color", colorString);
-		}
-
-		function setPageTopPaddingToHeight(height){
-			$("#page").css({
-				"padding-top":height
-			});
-		}
-
-		// Determine if the navigation is compatible with nav-fixed
-		var supportedFixedNavs = ["large-navigation-container-3"];
-		if ($(".nav-fixed").length > 0){
-			if (supportedFixedNavs.indexOf($(".nav-fixed").attr("id")) == -1){
-				alert("You have placed the class 'nav-fixed' on an element that does not support it.");
-			}else{
-				// Hook the events and call the function once for the page load
-				if ($(".nav-fixed").hasClass("interior")){
-					adjustNavFixedOpacity(true);
-					setPageTopPaddingToHeight($(".nav-fixed").height());
-					$(window).on("scroll", function(){
-						setPageTopPaddingToHeight($(".nav-fixed").height());
-					});
-					$(window).on("resize", function(){
-						setTimeout(function(){
-							setPageTopPaddingToHeight($(".nav-fixed").height());
-						}, 55);
-					});
-				}else{
-					$(window).on('scroll', adjustNavFixedOpacity);
-					adjustNavFixedOpacity();
-				}
-			}
-		}
-
-	// ***********************************
-	// Code for large-screen dropdowns
-	// ***********************************
-		/**
-		*	Because the scrollHeight property of an element includes absolute-positioned
-		*	children's height, this function exists to get the the total height of all
-		*	direct children.
-		*
-		*	@param jQuery jQueryDropDown - the jQuery object of the dropdown div
-		*/
-		function getTotalHeightOfAllDirectChildren(jQueryDropdown){
-			let totalHeight = 0;
-			jQueryDropdown.children().each(function(){
-				totalHeight += $(this).height();
-			});
-
-			return totalHeight;
-		}
-
-		$(".dropdown").parent()/*.children("a")*/.mouseenter(function(){
-			var container = $(this);//.parent();
-			var menu = container.children(".dropdown");
-			if (container.length > 0 && menu.css("visibility") === "hidden"){
-				// Dropdown exists
-				menu.css("visibility", "visible").css("height", 0).animate({height: getTotalHeightOfAllDirectChildren(menu)}, dropdownOpenSpeed);
-			}else if (menu.css("visibility") === "visible"){
-				// The dropdown is currently visible, but the mouse entered? This means it was trying to close.
-				// Close it immediately then open it again.
-				menu.css("visibility", "hidden");
-				menu.css("visibility", "visible").css("height", 0).animate({height: menu.get(0).scrollHeight}, dropdownOpenSpeed);
+		anime({
+			targets:tapDropdownContainer,
+			height:[0, childrenHeight],
+			duration:dropdownSpeed,
+			autoplay:true,
+			easing:"easeInOutSine",
+			complete:function(){
+				tapDropdownContainer.style.overflow = "unset";
+				tapDropdownContainer.style.height = "auto";
 			}
 		});
 
-		$(".dropdown").parent().children("a").each(function(index, element){
-			element = $(element); // Make the element a jQuery object
-			var container = element.parent();
-			var menu = container.children(".dropdown");
-			container.mouseleave(function(){
-				menu.css("visibility", "hidden");
-			})
+	}
+
+	/**
+	* Close a tap-dropdown
+	*
+	* @param {DOMNode} tapDropdownContainer - The tap-dropdown div
+	* @return {void}
+	*/
+	function closeTapDropdown(tapDropdownContainer){
+		tapDropdownContainer.style.overflow = "hidden";
+		let currentHeight = tapDropdownContainer.clientHeight;
+
+		// Must set the height from auto to the current computed height
+		tapDropdownContainer.style.height = currentHeight + "px";
+
+		anime({
+			targets:tapDropdownContainer,
+			height:[currentHeight, 0],
+			duration:dropdownSpeed,
+			autoplay:true,
+			easing:"easeInOutSine",
+			complete:function(){
+				tapDropdownContainer.style.overflow = "unset";
+				tapDropdownContainer.style.display = "none";
+			}
 		});
 
-	// ***********************************
-	// Code for small-screen dropdowns
-	// ***********************************
+	}
 
-	$(".tap-dropdown").parent().children("a").click(function(e){
-		e.preventDefault(); // Prevent being linked somewhere
+	// Connect and handle all large-nav dropdowns
+	for (let dropdown of allDropdowns){
+		let dropdownContainer = dropdown.parentNode;
 
-		var menu = $(this).parent().children(".tap-dropdown");
-		menu.toggle("fast");
+		dropdownContainer.onmouseleave = function(){
+			dropdown.style.visibility = "hidden";
+		}
 
-		// Close any sub-menus inside this sub-menu as well
-		menu.find(".tap-dropdown").hide();
+		dropdownContainer.onmouseenter = function(){
+			let dropdownVisibility = dropdown.style.visibility;
 
-		return false; // Prevent being directed somewhere on older browser (idk which)
-	});
-
-	// ***********************************
-	// Code for opening navigation menus
-	// ***********************************
-
-		// ***********************************
-		// Small navigation menu 1
-		// ***********************************
-
-			// Does this menu exist?
-
-			if ($("#small-navigation-container-1").length > 0){
-
-				function closeSmallMenu1(){
-					var backdrop = $("#small-navigation-container-1 .pullout-menu-backdrop");
-					var navContainer = backdrop.children("div");
-
-					// Close the menu
-					navContainer.css("margin-right", navContainer.attr('data-original-margin-right'));
-
-					setTimeout(function(){
-						backdrop.removeClass("d-flex");
-						backdrop.addClass("d-none");
-					}, 150); // The 150ms timeout here is because the CSS animation is set to 150ms. If you change the CSS animation speed also change the timeout here.
-				}
-
-				// Open hamburger pressed
-				$("#small-navigation-menu-opener-1").click(function(){
-					var container = $(this).parent().parent().parent();
-					var backdrop = container.find(".pullout-menu-backdrop"); // This is the transparent black backdrop
-
-					// Upon opening, remove d-none
-					backdrop.removeClass("d-none");
-
-					// Add d-flex
-					backdrop.addClass("d-flex");
-
-					// Get the actual container of the nav links
-					var navContainer = backdrop.children("div");
-					console.log(navContainer);
-
-					// Change the margin. CSS will animate it into view
-					navContainer.attr('data-original-margin-right', navContainer.css("margin-right")).css("margin-right", 0);
-				});
-
-				// Clicked on the transparent backdrop! Close the menu
-				$("#small-navigation-container-1").find(".pullout-menu-backdrop").click(function(){
-					closeSmallMenu1();
-				});
-
-				// Disallow bubbling
-				$("#small-navigation-container-1").find(".pullout-menu-backdrop").children("div").click(function(e){
-					// This stops the bubbling.
-					// JavaScript events bubble up through their parent elements.
-					// Without this code the event would bubble to the transparent backdrop and cause the menu to close whenever a user clicks or taps inside the actual menu.
-					// That is ... undesirable ...
-					if (e.target.tagName == "A"){
-						return;
+			if (dropdownVisibility === "hidden"){
+				// The dropdown was hidden, but needs to be open
+				dropdown.style.visibility = "visible";
+				dropdown.style.height = "0px";
+				dropdown.style.overflow = "hidden";
+				anime({
+					targets:dropdown,
+					height:[0, getTotalHeightOfAllDirectChildren(dropdown)],
+					duration:dropdownSpeed,
+					autoplay:true,
+					easing:"easeInOutSine",
+					complete:function(){
+						dropdown.style.overflow = "unset";
 					}
-					e.stopPropagation();
-					return false;
 				});
-
-				// Close the small menu on resize.
-				$(window).on('resize', function(){
-					// timeout because of the "window resize" event fires before the resize happens
-					setTimeout(function(){
-						// Check if it's open first.
-						if ($("#small-navigation-container-1 .pullout-menu-backdrop").is(":visible")){
-							closeSmallMenu1();
-						}
-					}, 50);
+			}else{
+				// The dropdown was showing, but needs to be hidden and then re-opened
+				dropdown.style.visibility = "hidden";
+				dropdown.style.visibility = "visible";
+				dropdown.style.height = "0px";
+				dropdown.style.overflow = "hidden";
+				anime({
+					targets:dropdown,
+					height:[0, getTotalHeightOfAllDirectChildren(dropdown)],
+					duration:dropdownSpeed,
+					autoplay:true,
+					easing:"easeInOutSine",
+					complete:function(){
+						dropdown.style.overflow = "unset";
+					}
 				});
-
 			}
-});
+		}
+	}
+
+	// Connect and handle all small-nav tap-dropdowns
+	for (let tapDropdown of allTapDropdowns){
+		let dropdownContainer = tapDropdown.parentNode;
+		let dropdownLinkActivator = dropdownContainer.getElementsByTagName("a")[0];
+
+		dropdownLinkActivator.onclick = function(){
+			let computedDisplayStyle = window.getComputedStyle(tapDropdown).getPropertyValue("display");
+			if (computedDisplayStyle === "none"){
+				openTapDropdown(tapDropdown);
+			}else{
+				closeTapDropdown(tapDropdown);
+			}
+		}
+	}
+
+	// Handle the opening of the small menu (#1)
+	if (smallNavContainer1 !== null){
+		let smallNavBackdrop = smallNavContainer1.getElementsByClassName("pullout-menu-backdrop")[0];
+		let smallNavOpener = document.getElementById("small-navigation-menu-opener-1");
+		let smallNavMenu = smallNavBackdrop.children[0];
+
+		/**
+		* Closes the small navigation menu 1
+		*
+		* @return {void}
+		*/
+		function closeSmallNavMenu1(){
+			let originalMarginRight = parseInt(smallNavMenu.getAttribute("data-original-margin-right"), 10);
+			smallNavMenu.style.marginRight = originalMarginRight + "px";
+			smallNavBackdrop.classList.remove("d-flex");
+			smallNavBackdrop.classList.add("d-none");
+		}
+
+		/**
+		* Opens the small navigation menu 1
+		*
+		* @return {void}
+		*/
+		function openSmallNavMenu1(){
+			let menuMarginRight = window.getComputedStyle(smallNavMenu).getPropertyValue("margin-right");
+
+			smallNavBackdrop.classList.remove("d-none");
+			smallNavBackdrop.classList.add("d-flex");
+
+			smallNavMenu.setAttribute("data-original-margin-right", menuMarginRight);
+			smallNavMenu.style.marginRight = "0px";
+		}
+
+		// Connect the opener click
+		smallNavOpener.onclick = function(){
+			openSmallNavMenu1();
+		}
+
+		// Connect to backdrop click
+		smallNavBackdrop.onclick = function(e){
+
+			// Only activate when the backdrop itself is clicked and this is not the result of bubbling
+			if (e.target === smallNavBackdrop){
+				closeSmallNavMenu1();
+			}
+		}
+	}
+})();

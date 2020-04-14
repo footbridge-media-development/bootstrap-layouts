d00cf6cb41a9eec9e3b89926a581b07f1d8545d0
nox7 <17110935+nox7@users.noreply.github.com>
2020-04-14 16:23:14 -0500

    Change .fullwindow to be completely CSS
    No need for JS here

--- layouts/js/full-top.js
+++ layouts/js/full-top.js
@@ -1,27 +1,3 @@
-$(function()
-{
-
-		if ($(window).width() > 992)
-		{
-			 $('.fullwindow') .css({'min-height': (($(window).height()))+'px'});
-		}
-		else {
-			$('.fullwindow') .css({'min-height': '400px'});
-		}
-
-
-        $(window).bind('resize', function(){
-			if ($(window).width() > 992)
-		{
-			 $('.fullwindow') .css({'min-height': (($(window).height()))+'px'});
-		}
-		else {
-			$('.fullwindow') .css({'min-height': '400px'});
-		}
-
-        });
-});
-
 $(".scroll-next").click(function() {
 	let cls = $(this).closest(".section").next().offset().top;
 	$("html, body").animate({scrollTop: cls}, "slow");

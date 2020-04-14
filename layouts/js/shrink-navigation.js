1e9ad8b54ab837b882ac7d8e325a977265ac8fce
nox7 <17110935+nox7@users.noreply.github.com>
2020-04-14 16:28:07 -0500

    Tabize JS

--- layouts/js/shrink-navigation.js
+++ layouts/js/shrink-navigation.js
@@ -5,37 +5,37 @@
 
 $(document).ready(function() {
 
-    $('#navigation-logo').css('width', '200px');
-    $('#navigation-logo').css('transition', 'all .5s ease');
+	$('#navigation-logo').css('width', '200px');
+	$('#navigation-logo').css('transition', 'all .5s ease');
 
-    $('#large-navigation-container-2').css('transition', 'all .5s ease');
-    $('#large-navigation-container-2').css('background', 'rgba(0, 0, 0, 0.5)');
+	$('#large-navigation-container-2').css('transition', 'all .5s ease');
+	$('#large-navigation-container-2').css('background', 'rgba(0, 0, 0, 0.5)');
 
-    $('#small-navigation-container-1').css('transition', 'all .5s ease');
-    $('#small-navigation-container-1').css('background', 'rgba(0, 0, 0, 0.5)');
+	$('#small-navigation-container-1').css('transition', 'all .5s ease');
+	$('#small-navigation-container-1').css('background', 'rgba(0, 0, 0, 0.5)');
 
-    if ($("#large-navigation-container-2").length !== 0) {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 250) {
+	if ($("#large-navigation-container-2").length !== 0) {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 250) {
 
-                $('#navigation-logo').css('width', '170px');
+				$('#navigation-logo').css('width', '170px');
 
-                $('#large-navigation-container-2').css('background-color', 'rgba(0, 0, 0, 1)');
+				$('#large-navigation-container-2').css('background-color', 'rgba(0, 0, 0, 1)');
 
-                $('#small-navigation-container-1').css('background-color', 'rgba(0, 0, 0, 1)');
+				$('#small-navigation-container-1').css('background-color', 'rgba(0, 0, 0, 1)');
 
-            }
-            else {
+			}
+			else {
 
-                $('#large-navigation-container-2').css('background', 'rgba(0, 0, 0, 0.5)');
+				$('#large-navigation-container-2').css('background', 'rgba(0, 0, 0, 0.5)');
 
-                $('#small-navigation-container-1').css('background', 'rgba(0, 0, 0, 0.5)');
+				$('#small-navigation-container-1').css('background', 'rgba(0, 0, 0, 0.5)');
 
-                $('#navigation-logo').css('width', '200px');
+				$('#navigation-logo').css('width', '200px');
 
-            }
-        });
-    }
+			}
+		});
+	}
 });
 
 /* ******************************************************************************************** */

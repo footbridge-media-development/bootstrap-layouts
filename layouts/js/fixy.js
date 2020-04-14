236ead8478b87c1bad53aaf95009929400a6cdbf
nox7 <17110935+nox7@users.noreply.github.com>
2020-04-14 16:27:02 -0500

    Format/document the fixy.js

--- layouts/js/fixy.js
+++ layouts/js/fixy.js
@@ -1,14 +1,13 @@
 $(window).scroll(function (event) {
-	var scroll = $(window).scrollTop();
+	let scroll = $(window).scrollTop();
 	if (scroll > 200) {
-		var navH = $("#large-navigation-container-2").outerHeight();
+		// Sets the height of the .pusher element to fix top-spacing
+		// for fixed-pos navs on interior pages
+		let navH = $("#large-navigation-container-2").outerHeight();
 		$("#large-navigation-container-2").addClass("turnME");
 		$(".pusher").css({"height": navH});
-	}
-		
-	else {
+	}else{
 		$("#large-navigation-container-2").removeClass("turnME");
 		$(".pusher").css({"height": ""});
-		}
-	
+	}
 });

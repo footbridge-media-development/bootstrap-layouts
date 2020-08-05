b1242242a8b258fc954616c93e9d41b405a42010
Ralph-Footbridge <36486888+Ralph-Footbridge@users.noreply.github.com>
2020-08-05 14:47:08 -0500

    new layout for better page speed
    This include, new large menu, use of webp and mobile images sizes for all images, deletion of font awesome, rolling all css and fontello into master theme

--- layouts/js/fixy.js
+++ layouts/js/fixy.js
@@ -1,3 +1,18 @@
+/*document.addEventListener("scroll", fixFunction);
+function fixFunction(){
+	let scroll = window.scrollTop;
+	if (scroll > 200) {
+		// Sets the height of the .pusher element to fix top-spacing
+		// for fixed-pos navs on interior pages
+		let navH = document.getElementById("#large-navigation-container-2").outerHeight();
+		document.getElementById("#large-navigation-container-2").classList.toggle("turnME");
+		document.getElementById(".pusher").css({"height": navH});
+	}else{
+		document.getElementById("#large-navigation-container-2").classList.toggle("turnME");
+		document.getElementById(".pusher").style.cssText({"height": ""});
+	}
+}
+*/
 $(window).scroll(function (event) {
 	let scroll = $(window).scrollTop();
 	if (scroll > 200) {

bb3d65a8b5dc531ac04d9b92de66adef1beacb67
Ralph-Footbridge <36486888+Ralph-Footbridge@users.noreply.github.com>
2020-09-08 14:08:06 -0500

    new css clean up and template
    Sarah's css clean up with some other updates to our base template.

--- layouts/js/fixy.js
+++ layouts/js/fixy.js
@@ -1,28 +1,10 @@
-/*document.addEventListener("scroll", fixFunction);
-function fixFunction(){
-	let scroll = window.scrollTop;
-	if (scroll > 200) {
-		// Sets the height of the .pusher element to fix top-spacing
-		// for fixed-pos navs on interior pages
-		let navH = document.getElementById("#large-navigation-container-2").outerHeight();
-		document.getElementById("#large-navigation-container-2").classList.toggle("turnME");
-		document.getElementById(".pusher").css({"height": navH});
-	}else{
-		document.getElementById("#large-navigation-container-2").classList.toggle("turnME");
-		document.getElementById(".pusher").style.cssText({"height": ""});
-	}
-}
-*/
-$(window).scroll(function (event) {
-	let scroll = $(window).scrollTop();
-	if (scroll > 200) {
-		// Sets the height of the .pusher element to fix top-spacing
-		// for fixed-pos navs on interior pages
-		let navH = $("#large-navigation-container-2").outerHeight();
-		$("#large-navigation-container-2").addClass("turnME");
-		$(".pusher").css({"height": navH});
-	}else{
-		$("#large-navigation-container-2").removeClass("turnME");
-		$(".pusher").css({"height": ""});
+const navigation = document.querySelector("nav");
+const navigationY = navigation.offsetHeight;
+
+window.addEventListener('scroll', function() {
+	if (window.pageYOffset > navigationY) {
+		navigation.classList.add("is-scrolled");
+	} else {
+		navigation.classList.remove("is-scrolled");
 	}
 });

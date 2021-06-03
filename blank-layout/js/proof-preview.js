31bb141f3e8143009d009d11616ae2a5df12cfbc
John Ellison <37640782+johnellison919@users.noreply.github.com>
2021-06-03 12:25:10 -0500

    Removes proof-preview.js
    Since the popup modal isn't used, we can remove the proof-preview.js

--- blank-layout/js/proof-preview.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", "a", function(e){
-	e.preventDefault();
-	e.stopPropagation();
-	if ($(this).hasClass("carousel-control-prev-icon")){
-		// Do not show the popup for carousel control arrows
-		return;
-	}
-	$("#preview-only-notice").modal("show");
-});

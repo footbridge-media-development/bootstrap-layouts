48fa716fc358cc5459978d2a0feb6a57e3a086b8
Garet C. Green <17110935+nox7@users.noreply.github.com>
2024-05-09 09:46:53 -0500

    Rewrite SVGIcon library JavaScript
    Removes unnecessary console.log writes and uses modern JavaScript

--- baseline-24.1/layout/js/svg-icons.js
+++ baseline-24.1/layout/js/svg-icons.js
@@ -1,32 +1,18 @@
-window.onload = function() {
-  svg_iosRender();
-}
-
-function svg_iosRender() {
-  const svIcons = document.querySelectorAll('.svicon');
-  svIcons.forEach(svIcon => {
-    const classes = Array.from(svIcon.classList);
-
-    let pathElement = null;
-    let matchingClass = null;
-
-    for (const className of classes) {
-      if (className !== 'svicon') {
-        const pathVariable = getComputedStyle(svIcon).getPropertyValue(`--${className}-path`);
-        if (pathVariable) {
-          const cleanedPath = pathVariable.trim().replaceAll('"', ''); // Remove any leading/trailing whitespaces
-          pathElement = svIcon.querySelector('path');
-          pathElement.setAttribute('d', cleanedPath);
-          matchingClass = className;
-          break; // Stop searching once a match is found
-        }
+window.addEventListener("load", () => {
+  const svIcons = document.querySelectorAll(".svicon");
+  for (const iconElement of svIcons){
+    // Get all classes that are not svicon
+    const unrelatedClassNames = Array.from(iconElement.classList).filter(className => className !== "svicon");
+    for (const className of unrelatedClassNames){
+      const pathVariable = window.getComputedStyle(iconElement).getPropertyValue(`--${className}-path`);
+      if (pathVariable !== ""){
+        // This class contains a CSS variable path
+        const cleanedPath = pathVariable.trim()
+          .replaceAll('"', "");
+        const pathElement = iconElement.querySelector("path");
+        pathElement.setAttribute("d", cleanedPath);
+        break;
       }
     }
-
-    if (matchingClass) {
-      console.log(`iOS Icon Path Found: ${matchingClass}`);
-    } else {
-      console.log('No matching icon path found');
-    }
-  });
-}
+  }
+});
